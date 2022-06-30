83673dcd7d98ec005370cccb9ddff9c9006e4de9
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-06-30 16:30:02 +0530

    Added: First name to the user registration

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -4,6 +4,8 @@ public class RegexMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.firstName();
 
     }
 }
---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    static Scanner scanner = new Scanner(System.in);
+    public void firstName() {
+        System.out.print("Enter First Name : ");
+        String firstName = scanner.next();
+        boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", firstName);
+        if (answer) {
+            System.out.println(firstName);
+
+        } else {
+            System.out.println("please enter valid first name ");
+
+            firstName();
+        }
+    }
+
+}
